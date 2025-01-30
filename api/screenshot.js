1649eb8f8da4668aead2ec2ac1faaf49a8d2e988
cecsdevelop <cecsdevelop@gmail.com>
2025-01-30 17:12:46 -0400

    reorder files

---
+++ api/screenshot.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método no permitido' });
+  }
+
+  try {
+    const { url } = req.body;
+
+    if (!url) {
+      return res.status(400).json({ error: 'Falta la URL' });
+    }
+
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    const page = await browser.newPage();
+    await page.goto(url, { waitUntil: 'networkidle2' });
+    const screenshotBuffer = await page.screenshot();
+    await browser.close();
+
+    res.setHeader('Content-Type', 'image/png');
+    res.status(200).send(screenshotBuffer);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error al generar la captura de pantalla' });
+  }
+}
