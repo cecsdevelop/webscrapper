ddf6b6fb5001725d74a5f48cfd545b1a3552934c
cecsdevelop <cecsdevelop@gmail.com>
2025-01-31 18:06:30 -0400

    new connection

--- api/index.js
+++ api/index.js
@@ -1,14 +1,12 @@
-const puppeteer = require('puppeteer-core');
+import puppeteer from 'puppeteer-core';
 
-(async () => {
-    const browser = await puppeteer.launch({
-        executablePath: '/usr/bin/google-chrome-stable', // Ruta en Render
-        args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        headless: true
-    });
+const browser = await puppeteer.launch({
+    executablePath: '/usr/bin/google-chrome-stable',
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    headless: true
+});
 
-    const page = await browser.newPage();
-    await page.goto('https://intelindev.com');
-    await page.waitForTimeout(5000);
-    await browser.close();
-})();
+const page = await browser.newPage();
+await page.goto('https://intelindev.com');
+await page.waitForTimeout(5000);
+await browser.close();
