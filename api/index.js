245e32993f1480d3a7d5448696723534f1545834
cecsdevelop <cecsdevelop@gmail.com>
2025-01-31 18:11:09 -0400

    new connection

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 import puppeteer from 'puppeteer-core';
 
 const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/google-chrome-stable',
+    executablePath: '/usr/bin/chromium-browser', // Usa Chromium en Render
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     headless: true
 });
 
 const page = await browser.newPage();
-await page.goto('https://intelindev.com');
+await page.goto('https://example.com');
 await page.waitForTimeout(5000);
 await browser.close();
